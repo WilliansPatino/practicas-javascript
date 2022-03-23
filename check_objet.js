89f93117f9af3ee47be1e990db4109d1180a2977
Willians Patiño <ppwj@yahoo>
2022-03-23 18:47:40 -0400

    Primer envio

---
+++ check_objet.js
@@ -0,0 +1,20 @@
+function checkObj(obj, checkProp) {
+  // Cambia solo el código debajo de esta línea
+
+if (obj.hasOwnProperty(checkPro)) {;
+    return obj.checkProp;
+  } else {
+
+    return "Not Found";
+  }
+  // Cambia solo el código encima de esta línea
+}
+
+const cosas = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+res1 = checkObj(cosas,"gift");
+console.log(res1);
